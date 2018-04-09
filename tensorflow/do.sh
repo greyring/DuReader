8712679f32166cb73aa18bc26218f27fe6bdf916
greyring <greyring@163.com>
2018-04-09 17:56:09 +0800

    add do.sh

---
+++ tensorflow/do.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+file_dir="../data/preprocessed"
+train_file="--train_files $file_dir/trainset/search.train.json $file_dir/trainset/zhidao.train.json"
+dev_file="--dev_files $file_dir/devset/search.dev.json $file_dir/devset/zhidao.dev.json"
+test_file="--test_files $file_dir/testset/search.test1.json $file_dir/testset/zhidao.test1.json"
+files="$train_file $dev_file $test_file"
+
+python run.py --prepare $files
+python run.py --train $files --algo BIDAF --epochs 10 --gpu 0
+python run.py --evaluate $files --algo BIDAF --gpu 0
+python run.py --predict $files --algo BIDAF --gpu 0
